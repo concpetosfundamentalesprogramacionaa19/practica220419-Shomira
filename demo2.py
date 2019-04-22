b31d3ac6c8348afa66280ed02c88ff183ee01087
Shomira <shomirarosales01@gmail.com>
2019-04-22 16:14:43 -0500

    Clase4

---
+++ demo2.py
@@ -0,0 +1,13 @@
+
+"""
+envio el 5 luehgo el 10 y el programa debe dar el resultado 
+"""
+import sys
+nombre = sys.argv[0]
+dato1 =  sys.argv[1]
+dato2 =  sys.argv[2]
+
+suma = int(dato1) + int(dato2) #Suma d evariables
+multiplicacion = int(dato1) * int(dato2) #Multiplicaicon de variables
+
+print("La suma es :  %d\t La multiplicacion es: %d" % (suma , multiplicacion))
---
+++ demo3.py
@@ -0,0 +1,12 @@
+"""Ingreso de dos numeros de tipo string y convertirlos a e
+nteros y mostarr la suma entre ellos y la multiplicacion
+"""
+
+print("Escriba el numero 1 : ")
+numero1 =input()
+print ("Escriba el numero 2 : ")
+numero2= input()
+suma = int(numero1) + int(numero2) #Suma de numeros convirtiendolos a enteros
+multiplicacion = int (numero1) * int(numero2) #Multiplicacion de numeros convirtiendolos a enteros 
+print("La suma es :  %d\t La Multiplicacion es: %d" % (suma , multiplicacion))
+
